ca3e8c2e26a97812d35c8365152b20fb159099d8
Victoria <mariavictoria0916@hotmail.com>
2019-05-07 11:10:30 -0500

    modified jenkinsfile and karma config : Pruebita #3

--- karma.conf.js
+++ karma.conf.js
@@ -23,12 +23,12 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
+    reporters: ['progress', 'kjhtml','junit'],
     junitReporter: {
       outputDir:'reports',
       outputFile: 'test-results-karma.xml',
       suite: ''
     },
-    reporters: ['progress', 'kjhtml','junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
